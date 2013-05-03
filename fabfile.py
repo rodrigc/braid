89474651b5c5f7f5f59cfc9c3ba28f7a299b030a
Tom Prince <tom.prince@ualberta.net>
2013-05-03 16:19:17 -0600

    Import service fabfiles into braid fabfile.

--- fabfile.py
+++ fabfile.py
@@ -13,5 +13,16 @@ Just experimenting for now.
 from braid import base, users
 from braid import config
 
-
 __all__ = ['base', 'config', 'users']
+
+from twisted.python.filepath import FilePath
+import imp
+p = FilePath(__file__).sibling('services')
+for service in p.children():
+    fabfile = service.child('fabfile.py')
+    if fabfile.exists():
+        module = imp.load_source(service.basename(), fabfile.path, fabfile.open())
+        if module.config == config:
+            del module.config
+        globals()[service.basename()] = module
+        __all__.append(service.basename())
