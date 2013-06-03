5781e61e0c48b3f5bf0ff15cf6c2bed4eac7c888
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-06-03 18:33:45 -0400

    Initial implementation of a generic call tracking decorator.
    This decorator checks that the repository in which the function was
    initially defined is clean, then calls the function and adds an
    annotated tag to the repository.
    
    The tracking has to be activated in the config.
    
    This is only an initial version, more configurability should be added,
    as well as a ready to use decorator to specifically track deployments.

--- braid/git.py
+++ braid/git.py
@@ -1,4 +1,10 @@
-from fabric.api import run, cd
+import functools
+import inspect
+import os
+import pipes
+import datetime
+
+from fabric.api import run, cd, lcd, local, env, hide, abort
 
 from braid import package, fails
 
@@ -16,3 +22,43 @@ def branch(branch, location):
         with cd(location):
             run('/usr/bin/git fetch origin')
             run('/usr/bin/git reset --hard origin')
+
+
+def track_calls():
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            track = getattr(env, 'track_calls', False)
+            if track:
+                # Get the repository where the function is defined
+                directory = os.path.dirname(inspect.getsourcefile(func))
+
+                # Check that the repository is clean
+                with hide('running'), lcd(directory):
+                    repo = local('git rev-parse --show-toplevel', capture=True)
+
+                    with lcd(repo):
+                        changes = local('git status --porcelain', capture=True)
+
+                if changes:
+                    print (
+                        '\nCalls to the task your are running are required to '
+                        'be tracked, \nbut your repository is not clean. '
+                        'Please commit the changes to\nthe files in the '
+                        'following repository before deploying:\n'
+                    )
+                    print repo
+                    for line in changes.splitlines():
+                        print '  - ' + changes
+                    abort('Dirty repository')
+            result = func(*args, **kwargs)
+            if track:
+                tag_name = 'deploy-{:%Y-%m-%d-%H%M}'.format(datetime.datetime.now())
+                tag_message = 'Deployed to {}.'.format(env.host)
+
+                with lcd(repo):
+                    local('git tag -a {} -m {}'.format(
+                        pipes.quote(tag_name), pipes.quote(tag_message)))
+            return result
+        return wrapped
+    return decorator
